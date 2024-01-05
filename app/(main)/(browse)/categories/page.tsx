be4aa0d0f19ad5cedebcd19b6f5a126005741a67
Trivial-lore <triviallore@gmail.com>
2024-01-05 08:40:54 -0600

    fixed responsiveness of categories

--- app/(main)/(browse)/categories/page.tsx
+++ app/(main)/(browse)/categories/page.tsx
@@ -11,12 +11,12 @@ const CategoryPage = () => {
 <div className="flex items-center  flex-col min-h-screen h-auto bg-[url(/backgrounds/spookybg4.png)] bg-no-repeat bg-cover bg-fixed bg-center w-full ">
   
 <Particles className="fixed inset-0 z-10 " />
-<div className="h-[150px] bg-slate-900/50 w-full flex flex-col items-center justify-center gap-2">
-  <h1 className="font-bold text-5xl">Our Wicked and Wonderful World</h1>
-    <p className="text-lg">Browse through the categories to find the articles you are interested in, or use the searchbar.</p>
+<div className="h-[150px] bg-slate-900/50 w-full flex flex-col items-center justify-center gap-2 p-2 pt-5 pb-5">
+  <h1 className="font-bold text-2xl lg:text-5xl">Our Wicked and Wonderful World</h1>
+    <p className="text-mdmd:text-lg px-2">Browse through the categories to find the articles you are interested in, or use the searchbar.</p>
     
 </div>
-<div className="min-h-screen bg-slate-900/50 w-full grid grid-cols-3 gap-2 gap-y-2 pb-5">
+<div className="min-h-screen bg-slate-900/50 w-full grid md:grid-cols-2 lg:grid-cols-3 gap-2 gap-y-2 pb-5">
    
       <CategoryCard/>
       <CategoryCard/>
