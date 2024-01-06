acf93a0aa0e7186a49b80cf1abfafc39082ebc3c
Trivial-lore <triviallore@gmail.com>
2024-01-06 17:01:34 -0600

    fixed categories element overflow

--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -4,10 +4,10 @@ import Link from "next/link"
 
 export const Footer = () => {
   return (
-    <div className="block bottom-0 w-full    bg-gradient-to-r from-fuchsia-600/40 to-purple-900/60 bg-opacity-50">
-      <div className=" mx-auto flex items-center w-full bg-slate-900/40 h-full justify-between p-4">
+    <div className="block bottom-0 w-full   bg-gradient-to-r from-fuchsia-600/40 to-purple-900/60 bg-opacity-50">
+      <div className="  flex items-center w-full bg-slate-900/40 h-full justify-between p-4">
        <div className="hidden md:flex"> <Logo/></div>
-        <div className="space-x-4 md:blockm md:w-auto flex items-center justify-between w-full">
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button className="text-black dark:text-white/80" size="sm" variant="ghost">
             Privacy Policy
           </Button>
--- app/(main)/(browse)/categories/page.tsx
+++ app/(main)/(browse)/categories/page.tsx
@@ -18,7 +18,7 @@ const CategoryPage = () => {
   imageUrl="/headers/spnobg2.png"
   />
 
-<div className="h-auto min-h-screen bg-slate-900/80 w-full grid md:grid-cols-2 lg:grid-cols-3 gap-2 gap-y-2 pb-5 pt-5">
+<div className=" min-h-full bg-slate-900/80  w-full grid md:grid-cols-2 lg:grid-cols-3 gap-2 gap-y-2 pb-5 pt-5 px-5">
    
       <CategoryCard/>
       <CategoryCard/>
@@ -26,6 +26,7 @@ const CategoryPage = () => {
       <CategoryCard/>
       <CategoryCard/>
       <CategoryCard/>
+      
       
 </div>
 
