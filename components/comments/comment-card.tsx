a1170c4c08b15a224819b5e933ea9dc64e5ad207
Trivial-lore <triviallore@gmail.com>
2024-01-08 08:23:14 -0600

    fixed

--- components/comments/comment-card.tsx
+++ components/comments/comment-card.tsx
@@ -53,8 +53,8 @@ const CommentCard = ({
           <p>0</p>
           </div>
         </div>
-        <Button variant="green" asChild>
-        <Link className="flex items-center justify-center gap-1 hover:scale-105 z-10 bg-gradient-to-br  from-purple-600/40 to-emerald-900/60" href="/">
+        <Button className="flex items-center justify-center gap-1 hover:scale-105 z-10 bg-gradient-to-br  from-purple-600/40 to-emerald-900/60" variant="green" asChild>
+        <Link href="/">
           Reply <ArrowRight className="h-4 w-4"/>
         </Link>
         </Button>
