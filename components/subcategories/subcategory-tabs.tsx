31cc90a1cbf0d6e831d1747010c904541c2612f9
Trivial-lore <triviallore@gmail.com>
2024-01-08 13:59:37 -0600

    avatar bg updates, and subcategory tabs

--- components/subcategories/subcategory-tabs.tsx
+++ components/subcategories/subcategory-tabs.tsx
@@ -19,9 +19,9 @@ return null;
   return (
     <Tabs defaultValue="subcategories" className="w-full bg-slate-900/80">
   <TabsList className="bg-gradient-to-l  from-emerald-600/30 via-purple-700/50 to-emerald-900/60 opacity-80 flex flex-col md:flex-row rounded-none items-center justify-center w-auto h-auto ">
-    <TabsTrigger className="hover:bg-emerald-400/50 hover:text-purple-700 text-emerald-400 z-10 data-[state=active]:text-emerald-300 data-[state=active]:bg-purple-600/70" value="subcategory1">Subcategory1</TabsTrigger>
-    <TabsTrigger className="hover:bg-emerald-400/50 hover:text-purple-700 text-emerald-400 z-10 data-[state=active]:text-emerald-300 data-[state=active]:bg-purple-600/70"value="subcategory2">Subcategory2</TabsTrigger>
-    <TabsTrigger className="hover:bg-emerald-400/50 hover:text-purple-700 text-emerald-400 z-10 data-[state=active]:text-emerald-300 data-[state=active]:bg-purple-600/70"value="subcategory3">Subcategory3</TabsTrigger>
+    <TabsTrigger className="hover:bg-purple-400/40 hover:text-emerald-200 text-emerald-400 z-10 data-[state=active]:text-emerald-300 data-[state=active]:bg-purple-600/70" value="subcategory1">Subcategory1</TabsTrigger>
+    <TabsTrigger className="hover:bg-purple-400/40 hover:text-emerald-200 text-emerald-400 z-10 data-[state=active]:text-emerald-300 data-[state=active]:bg-purple-600/70"value="subcategory2">Subcategory2</TabsTrigger>
+    <TabsTrigger className="hover:bg-purple-400/40 hover:text-emerald-200 text-emerald-400 z-10 data-[state=active]:text-emerald-300 data-[state=active]:bg-purple-600/70"value="subcategory3">Subcategory3</TabsTrigger>
   </TabsList>
   {/* <p className="text-sm p-1 text-purple-300/80 text-center">Select a subcategory tab to view and search all the articles in it.</p> */}
   <TabsContent value="subcategory1">
