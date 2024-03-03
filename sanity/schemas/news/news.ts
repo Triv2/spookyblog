6e55d9fedba05911d492445b36ed5364560ad415
Trivial-lore <triviallore@gmail.com>
2024-03-03 07:47:24 -0600

    new schema updated

--- sanity/schemas/news/news.ts
+++ sanity/schemas/news/news.ts
@@ -1,46 +1,44 @@
-import {defineField, defineType} from 'sanity'
+import { defineField } from "sanity";
 
-export default defineType({
-  name: 'news',
-  title: 'News',
-  type: 'document',
+const news = {
+  name: "news",
+  title: "News",
+  type: "document",
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
       },
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      // @ts-ignore
-      to: {type: 'author'},
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
     }),
     defineField({
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "image",
       options: {
         hotspot: true,
       },
-      // @ts-ignore
       fields: [
         {
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative Text',
-        }
-      ]
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
     }),
     defineField({
       name: "link",
@@ -48,18 +46,17 @@ export default defineType({
       type: "string",
     }),
 
-
     defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
     }),
     defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'content',
+      name: "body",
+      title: "Body",
+      type: "content",
     }),
   ],
+};
 
-  
-})
+export default news;
